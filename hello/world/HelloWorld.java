a2ff61a764e88cdacd2bf7de9f0547a3a14a3499
Joe Isca <joeisca@gmail.com>
2014-12-04 09:19:40 -0600

    auto

--- hello/world/HelloWorld.java
+++ hello/world/HelloWorld.java
@@ -15,7 +15,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World again!");
+        System.out.println("Hello World Auto!");
     }
     
 }
